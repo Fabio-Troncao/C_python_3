fd607e1ce25c782bb7e0cd3ab6b6bb4fb31a14ce
Fábio Troncão <fabiotroncao@gmail.com>
2022-12-23 23:28:31 -0300

    PY4_comp

---
+++ aula3.py
@@ -0,0 +1,30 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte 
+Tipo de tipagem = Dinâmica -> Py ja sabe o tipo da informacao passada para ele.
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+
+print('Bernardo Pereira')
+# Forma de aparecer aspas duplas na string
+print(1, 'Bernardo "Pereira"')
+
+# Aspas duplas
+
+print("Bernardo Pereira")
+# Forma para aparecer aspas simples na string
+print(2, "Bernardo 'Pereira'")
+
+
+# Escape
+
+print('Bernardo \"Pereira')
+
+# r
+
+print(r'Bernardo \"Pereira')
