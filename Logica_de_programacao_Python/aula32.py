9f4b2b3c2c7d2fa10ba870d759a4b93f23301c73
Fábio Troncão <fabiotroncao@gmail.com>
2023-02-06 21:38:05 -0300

    Logica32

--- Logica_de_programacao_Python/aula32.py
+++ Logica_de_programacao_Python/aula32.py
@@ -15,3 +15,38 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
+
+"""numero_int = input('Digite um numero inteiro: ')
+
+try:
+    numero_int = int(numero_int)
+    if numero_int %2 == 0:
+        print('Seu numero é par.')
+    else:
+        print('Seu numero é ímpar')
+except:
+    print('O que digitou não é um número inteiro')"""
+
+"""hora = input('Digite a hora: ')
+hora = int(hora)
+bom_dia = hora >= 0 and hora <= 11
+boa_tarde = hora >= 12 and hora <= 17
+boa_noite = hora >= 18 and hora <= 23
+
+if bom_dia:
+    print('Bom Dia!!!')
+elif boa_tarde:
+    print('Boa Tarde!!!')
+elif boa_noite:
+    print('Boa noite!!!')
+else:
+    print('Informação digitada incorreta!!!')"""
+
+nome = input('Digite seu primeiro nome: ')
+
+if len(nome) <= 4:
+    print("Seu nome é curto")
+elif len(nome) >=5 and len(nome) <= 6:
+    print("Seu nome é normal")
+else:
+    print("Seu nome é muito grande")
