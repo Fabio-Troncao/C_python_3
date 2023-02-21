ff3c7f139e7bbbd4b9c1f5b853099e6b570dc70e
Fábio Troncão <fabiotroncao@gmail.com>
2023-02-21 03:08:52 -0300

    Logica39_corr

--- Logica_de_programacao_Python/aula39.py
+++ Logica_de_programacao_Python/aula39.py
@@ -7,3 +7,11 @@ nome = 'Fabio Troncao' # Iteráveis
 
 # Usar printe para confirmar, no caso de Duvida.
 print(nome[-11]) # usando -11 por exemplo ele vai contar de frente para traz sendo o -11 = b
+
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+print(nome[3])
+
+nova_string = ''
+nova_string += '*F*a*b*i*o* *T*r*o*n*c*a*o'
